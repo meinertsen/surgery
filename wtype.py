3ecfdcd3443fc9fee2c230dddf312c4f2529c8a0
Frederik Meinertsen <meinertsen@gmail.com>
2017-10-19 00:10:50 +0200

    Update wtype.py

--- wtype.py
+++ wtype.py
@@ -3,9 +3,9 @@ import builtins
 
 def wtype(object):
     test=dir(object)
-    return [b[1] for b in 
+    return [b[:2] for b in 
            [i for j in [[[k.__name__,i,dir(getattr(k,i))]
               for i in dir(k) if isinstance(getattr(k,i),type)] 
-              for k in (types,builtins)] 
+              for k in set([j for i, j in globals().items() if isinstance(j, types.ModuleType)])] 
               for i in j] if test == b[2]
           ]
