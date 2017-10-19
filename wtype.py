d3f5cae160b45255bb317084ffc8e510ba7b8a0f
Frederik Meinertsen <meinertsen@gmail.com>
2017-10-19 11:12:47 +0200

    Update wtype.py

--- wtype.py
+++ wtype.py
@@ -3,9 +3,25 @@ import builtins
 
 def wtype(object):
     return [h[:2] for h in 
-           [i for j in [[[mod.__name__,k,dir(getattr(mod,k))]
-              for k in dir(mod) if isinstance(getattr(mod,k),type)] 
-              for mod in set([n for m, n in globals().items() if isinstance(n, types.ModuleType)])] 
-              for i in j] if dir(object) == h[2]
+               [i for j in # merge lists
+                    [[[mod.__name__,k,dir(getattr(mod,k))]
+                        for k in dir(mod) 
+                            if isinstance(getattr(mod,k),type)] 
+                        for mod in set([n for m, n in globals().items() if isinstance(n, types.ModuleType)])] 
+               for i in j] # merge lists
+            if set(h[2]) <= set(dir(object)) #else set(h[2]).issubset(set(dir(object))) #
           ]
-wtype(object)
+
+    
+def wis(object):
+    def mergelists(ListofLists):
+        return [i for j in ListofLists for i in j]
+    
+    return [h[:3] for h in
+                mergelists(
+                  [[[mod.__name__,k,getattr(mod,k)]
+                          for k in dir(mod) if isinstance(getattr(mod,k),type)] 
+                          for mod in set([n for m, n in globals().items() if isinstance(n, types.ModuleType)])] 
+                )
+            if isinstance(object,h[2])
+           ]
